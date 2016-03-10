9f53c82c8538e8b4c045291a379ddffe8c945d31
allentdo <867652597@qq.com>
2016-03-10 22:09:45 +0800

    fix jobServer

--- BigData/src/com/zero/test/TestJobConn.java
+++ BigData/src/com/zero/test/TestJobConn.java
@@ -4,7 +4,26 @@ import com.zero.utils.JobServiceCon;
 import com.zero.utils.JobServiceCon;
 
 public class TestJobConn {
-	public static void main(String[] args) {
-		System.out.println(JobServiceCon.startContextConnectionApp("sc11", "sys11"));
+	public static void main(String[] args) throws Exception {
+		// System.out.println(JobServiceCon.startContextConnectionApp("sc11","sys11"));
+		// System.out.println(JobServiceCon.startContextConnectionApp("sc12","sys12"));
+		// String
+		// [][]arr=JobServiceCon.runMakedataJobAndGetDoubleArr2("sc12",1000,"/tmp/makedata2/admin2016332",
+		// "admin2016332", 20);
+
+		// String
+		// arr=JobServiceCon.runMakedataJob1("sc11",1000,"/tmp/makedata/admin123456","admin123456","/tmp/modelsave/model","1s100s1s10000s1s10000",40);
+		String[][] arr = JobServiceCon.runMakedataJobAndGetDoubleArr1("sc11",
+				10000000, "/tmp/makedata/admin123", "admin123",
+				"/tmp/modelsave/model", "1s100s1s10000s1s10000", 40);
+		// System.out.println(arr);
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " , ");
+			}
+			System.out.println();
+		}
+
 	}
 }
